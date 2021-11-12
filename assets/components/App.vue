552d3a7bd178b9a1a57929d394c4b39e31e1e734
carmodysan <carmodysan@outlookcom>
2021-11-12 23:26:43 +0100

    Ajout de Sass Modification de la barre de menu

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,6 +1,9 @@
 <template>
     <v-app v-cloak>
-        <v-app-bar short app >Symfony + Vuetify</v-app-bar>
+        <v-app-bar short clipped-left app >
+            <v-app-bar-nav-icon></v-app-bar-nav-icon>
+            <v-toolbar-title>Mon site perso</v-toolbar-title>
+        </v-app-bar>
         <v-main>
             <v-container fluid>
                 <router-view></router-view>
@@ -9,8 +12,14 @@
     </v-app>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>
 
 <script>
