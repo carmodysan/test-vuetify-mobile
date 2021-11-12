dec7db1c53219c4c432a8af9dd37f410580a9523
carmodysan <carmodysan@outlookcom>
2021-11-12 23:02:14 +0100

    Ajout de Vue, Vuetify

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,30 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import vuetify from './plugins/vuetify'
+
+import App from './components/App'
+import Home from './components/Home'
+
+const routes = [
+    { path: '/home', component: Home, name: 'home' }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: '/',
+    routes
+})
+
+Vue.use(VueRouter)
+
+new Vue({
+    router,
+    vuetify,
+    render: h => h(App)
+}).$mount('#app')
---
+++ assets/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app v-cloak>
+        <v-app-bar short app >Symfony + Vuetify</v-app-bar>
+        <v-main>
+            <v-container fluid>
+                <router-view></router-view>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<style scoped>
+
+</style>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
